3246630fa6660127f13b632e9dd47fadfdd43daf
Carlos Falcon <ing.falconc@gmail.com>
2023-02-12 23:17:04 -0300

    Pre Entrega 2 - Estructura, variables y objetos

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ function calcular_remesa(monto) {
     let remesa = 0;
 
     if (monto > 0) {
-        remesa = monto * 26;
+        remesa = monto * 28;
         return remesa
     }
 }
@@ -27,24 +27,100 @@ function es_usuario(remesa, usuario) {
 
 alert("Bienvenido/a a Remesas Free (Cambio de Pesos CLP a Bolívares VES)");
 
-let monto = "";
 
+class Moneda{
+    constructor( tipo, stock){
 
-while (monto != "FIN") {
-
-    monto = prompt("Ingrese el monto a enviar en CLP o ingrese FIN para finalizar");
-
-    if (monto != "FIN") {
-        let usuario = prompt("Es usuario Premium: SI o NO");
-        let total = calcular_remesa(monto);
-        let descuento = es_usuario(total, usuario);
-        let total_descuento = monto - descuento;
-
-        alert("Monto que envías en CLP (Tasa: CLP 1 = VES 0.026): $" + monto 
-        + "\n\rDescuento usuario Premium (5%): $" + descuento
-        + "\n\rTotal a pagar: $" + total_descuento
-        + "\n\rEl destinatario recibe en VES: " + total);
-
-        monto = "FIN";
+            this.tipo = tipo;
+            this.stock = stock;
     }
+
+    get_datos(){
+        console.log("---------------");
+        console.log("Tipo: ", this.tipo);
+        console.log("Stock: ", this.stock);
+        console.log();
+    }
+
+    get_stock(){
+
+        if( this.stock <= 0){
+            return false
+        }
+        else{
+            return true
+        }
+    }
+
+    venta_stock( cantidad ){
+
+        if( this.stock >= cantidad ){
+            //VENTA
+
+            this.stock = this.stock - cantidad;
+            return true
+        }
+        else{
+            return false
+        }
+    }
+
 }
+
+// CARGA DE MONEDAS //
+
+let lista_monedas = [];
+
+lista_monedas.push( new Moneda("Bolivar", 5000000));
+
+// FIN CARGA DE MONEDA //
+
+// INICIO RENDER DE MONEDAS //
+
+for( let moneda of lista_monedas){
+
+    moneda.get_datos();
+}
+
+// FIN RENDER DE MONEDAS //
+
+function buscar_moneda( moneda ){
+    return moneda.tipo == compra_usuario
+}
+
+    let compra_usuario = prompt("Ingrese el tipo de moneda que desea enviar: ");
+    let resultado_find = lista_monedas.find( buscar_moneda );
+    console.log( resultado_find);
+
+    if ( resultado_find !=undefined ){
+
+        // COMPRA //
+        if( resultado_find.get_stock() ){
+            let unidades = prompt("¿Cuantos Bolivares deseas enviar?");            
+                   
+            if( resultado_find.venta_stock (unidades) ){
+            monto = prompt("Ingrese el monto a enviar en CLP");        
+            let usuario = prompt("Es usuario Premium: SI o NO");
+            let total = calcular_remesa(monto);
+            let descuento = es_usuario(total, usuario);
+            let total_descuento = monto - descuento;
+
+            console.log("Monto que envías en CLP (Tasa: CLP 1 = VES 0.028): $" + monto 
+            + "\n\rDescuento usuario Premium (5%): $" + descuento
+            + "\n\rTotal a pagar: $" + total_descuento
+            + "\n\rEl destinatario recibe en VES: " + total);
+            console.log("Gracias por la compra");
+        }
+        else{
+        console.log("No contamos con el monto solicitado");
+        }
+
+        }
+        else{
+        console.log("No se puede realizar la transacción");
+        }
+        resultado_find.get_datos();
+        }
+        else{
+            console.log("Moneda no encontrada: ", compra_usuario);
+            }
